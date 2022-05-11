724f913378424f41c1f930e8ad377042c42ae52f
le dung <dungltph14568@gmail.com>
2022-05-11 14:22:46 +0700

    first commit

---
+++ chap01/callback.js
@@ -0,0 +1,2 @@
+// forEach, map, 
+//callback là 1 tham số đc trả về 
---
+++ chap01/variables.js
@@ -0,0 +1,35 @@
+// var a = 10 ;
+// var a = 20;
+// var b = 20
+// console.log(a+b);
+
+// let a = 10 ;
+//  a = 20;
+// let b = 20
+// console.log(a+b);
+
+
+// const a = [1,2,3];
+// a.push(4);
+// // const b = 20 ;
+// console.log(a);
+// tham chiếu vào chung 1 cái
+// tham trị là 2 obj có cùng giá trị 
+// const a = { name : "Lấm"};
+// const b = a;
+// b.name = "Dung";
+// console.log(a);
+//  tham chiếu và tham trị 
+let numA = 20;
+let numB = 20 ;
+console.log(numA == numB);
+
+const a = { name: "lam"};
+const b = { name: "lam"}; //2 đối tượng độc lập k cùng giá trị
+
+// Note: 
+/*
+    -không sdung var
+    khuyến khích dùng const khai báo
+    DÙng let khi giá trị thay đổi
+*/ 
